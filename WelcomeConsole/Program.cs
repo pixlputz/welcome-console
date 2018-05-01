c9d58f00d65e0e7b8b758228333a873da2bbc714
pixlputz <pixlputz@gmail.com>
2018-04-30 21:00:34 -0600

    welcome msg

--- WelcomeConsole/Program.cs
+++ WelcomeConsole/Program.cs
@@ -10,6 +10,8 @@ namespace WelcomeConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
